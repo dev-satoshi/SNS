eb441d4ea8ac724fcdff7e3b792289095e69be71
satoshi <dev.satoshi.1016@gmail.com>
2023-08-31 20:14:01 +0900

    add: アプリのベースを作成

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import static
+from django.urls import include, path
+
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("timeline.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
